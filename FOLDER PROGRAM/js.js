ae2fb1b40b642a2ea546b561da6fff7e1064763e
ekaamelya <ekaamelya48@gmail.com>
2024-01-05 17:59:11 +0700

    eka: menambahkan absensi

--- FOLDER PROGRAM/js.js
+++ FOLDER PROGRAM/js.js
@@ -325,3 +325,27 @@ $('#tombolHapusData').on('click',function(){
 //         }
 //     }); 
 // })
+
+var formAb = document.querySelector(".form-absensi");
+var footerD = document.querySelector('.footerD');
+
+// if(localStorage.getItem('theme') == 'tDark')
+// 	darkmode(true)
+
+function darkmode(isDark) 
+{
+	if (isDark) {
+		document.body.setAttribute('id','dark');
+		formAb.classList.remove("shadow-lg");
+		formAb.setAttribute('id', 'bshadow');
+		footerD.classList.toggle('d-none');
+		// localStorage.setItem('theme', 'tDark');
+	}
+	else {
+		document.body.setAttribute('id', '');
+		formAb.classList.add("shadow-lg");
+		formAb.setAttribute('id', '');
+		footerD.classList.toggle('d-none');
+		// localStorage.setItem('theme', '');
+	}
+}
